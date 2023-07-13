e8a45e70b7a5f1b85502357826d7c77a545eeff6
Ankush Bahekar <ankushmb9@gmail.com>
2023-07-13 14:49:12 +0530

    [Ankush] Add:Reversing the elements order in Linked list.

--- LinkedList/ListData.cs
+++ LinkedList/ListData.cs
@@ -8,15 +8,53 @@ namespace LinkedList
 {
     public class ListData
     {
-        public void Add()
+        public Node head;
+        public void Add(int data)
         {
-            LinkedList<int> list = new LinkedList<int>();
-            list.AddFirst(56);
-            list.AddFirst(30);
-            list.AddFirst(70);
-            foreach (int val in list)
+            Node node = new Node(data);
+            if (head == null)
             {
-                Console.WriteLine(val);
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(data);
+        }
+        public void Reverse(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+
+            {
+                Node temp = head;
+                head = node;
+                head.next = temp;
+
+            }
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("List is Empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
             }
         }
     }
