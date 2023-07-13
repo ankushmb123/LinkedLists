a7917b21deecde20d64c872720d8dce9b081e323
Ankush Bahekar <ankushmb9@gmail.com>
2023-07-13 14:37:01 +0530

    [Ankush] Add:Create a Simple LinkedList 56, 30, 70.

---
+++ LinkedList/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        // initialize vriable int data
+        public int data;
+        // Node next
+        public Node next;
+        //Constructor to initilize data variables
+        public Node(int data)
+        {
+            this.data = data;
+        }
+        // end:: public  Node(int data)
+        // public class Node
+    }
+}
+
+
